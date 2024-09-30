0e599c93dfc19976de1c099eaa16d615e91515f5
CoderVivek92 <vivekgahlot92@gmail.com>
2024-09-30 14:18:17 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,10 +1,6 @@
 - 👋 Hi, I’m @Vivek9214
-- 👀 I’m interested in application
-- 🌱 I’m currently learning c++
+- 👀 I’m interested in web development
+- 🌱 I’m currently learning css,javascript,html
 - 💞️ I’m looking to collaborate on 
-- 📫 How to reach me ...
 
-<!---
-Vivek9214/Vivek9214 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+<!------------------------------------------------------------------------------------------------------------------->
